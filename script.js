774b9de6afdeb28aace2482ce2467ff598d98a50
Daria <daryay0@yandex.ru>
2022-05-02 23:36:38 +0300

    Первый комИт

---
+++ script.js
@@ -0,0 +1,7 @@
+/*jshint esversion: 6 */
+//const answers =  [];
+//answers[0] = prompt ("Паша помоет посуду?","");
+//answers[1] = prompt ("Паша подметет пол?","");
+//answers[2] = prompt ("Паша любит Дашу?","");
+
+//document.write(answers);
